94fedb64451ed8310e373ed86e82cf36da5a0493
Steven <stevenren999@gmail.com>
2023-05-14 05:10:12 -0700

    credited myself for programming

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -21,6 +21,7 @@ class Credits extends Phaser.Scene {
         menuConfig.fontSize='28px';
         this.add.text(game.config.width/2,game.config.height/4+50+50+50, "Game Over sfx by Pixabay", menuConfig).setOrigin(0.5,0.5);
         this.add.text(game.config.width/2,game.config.height/4+50+50+50+50, "All art by me", menuConfig).setOrigin(0.5,0.5);
+        this.add.text(game.config.width/2,game.config.height/4+50+50+50+50+50, "Programmed by me", menuConfig).setOrigin(0.5,0.5);
         this.add.text(game.config.width/2,game.config.height*9/10, "Press Enter to go back to menu", menuConfig).setOrigin(0.5,0.5);
         keyENTER = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
     }
